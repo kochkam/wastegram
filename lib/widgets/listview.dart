574eee90c843923e9db9d20b8d266972a80e3ace
Michael <mjkochka@gmail.com>
2021-03-14 18:44:04 -0700

    second

--- lib/widgets/listview.dart
+++ lib/widgets/listview.dart
@@ -3,53 +3,51 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import '../screens/detailview.dart';
 
-class List extends StatelessWidget{
-
+class List extends StatelessWidget {
   @override
-  build(BuildContext context){
+  build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('posts').orderBy('date',descending: true).snapshots(),
-      builder: (context,snapshot){
-        if(snapshot.hasData && snapshot.data.docs != null && snapshot.data.docs.length > 0){
+      stream: FirebaseFirestore.instance
+          .collection('posts')
+          .orderBy('date', descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData &&
+            snapshot.data.docs != null &&
+            snapshot.data.docs.length > 0) {
           return myresults(snapshot);
-        }
-        else{
+        } else {
           return Center(child: CircularProgressIndicator());
         }
-      },);
-
-
-
+      },
+    );
   }
 
-
-
-
-myresults(snapshot){
-  return ListView.builder(
-    itemCount: snapshot.data.docs.length,
-    itemBuilder: (context,index){
-      var post = snapshot.data.docs[index];
-      var datestring = post['date'].toDate().toString();
-      DateFormat dateFormat = DateFormat("yyyy-MM-dd");
-      DateTime datetime = dateFormat.parse(datestring); 
-      var formatter = new DateFormat('EEEE, MMMM d, y').format(datetime);
-      return Semantics(
-        label:"Tap for details",
-        onLongPressHint: "Tap for details",
-              child: ListTile(
-          title: Text(formatter),
-          trailing: Text(post['quantity'].toString()),
-          onTap: () => { Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => NewDetails(post)))},
-        ),
-      );
-    },
-  );
-
+  myresults(snapshot) {
+    return ListView.builder(
+      itemCount: snapshot.data.docs.length,
+      itemBuilder: (context, index) {
+        var post = snapshot.data.docs[index];
+        var datestring = post['date'].toDate().toString();
+        DateFormat dateFormat = DateFormat("yyyy-MM-dd");
+        DateTime datetime = dateFormat.parse(datestring);
+        var formatter = new DateFormat('EEEE, MMMM d, y').format(datetime);
+        return Semantics(
+          label: "Tap for details",
+          onLongPressHint: "Tap for details",
+          child: Card(
+            color: Colors.teal[300],
+            child: ListTile(
+              title: Text(formatter),
+              trailing: Text(post['quantity'].toString()),
+              onTap: () => {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => NewDetails(post)))
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
-
-
-
-
-}
-
--- lib/widgets/scrollabledetails.dart
+++ lib/widgets/scrollabledetails.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 
-
-
-class Details extends StatelessWidget{
-  final post; 
+class Details extends StatelessWidget {
+  final post;
   Details(this.post);
   @override
-  build(BuildContext context){
-     return Expanded(
+  build(BuildContext context) {
+    return Expanded(
       child: SingleChildScrollView(
         child: Container(
           width: 300.00,
           height: 300.00,
-          child: Column(children:[
-                  SizedBox(height:50.00),
-                  Text(post['quantity'].toString() + " items", style: TextStyle(fontSize: 40.00,color: Colors.white)),
-                  SizedBox(height:75.00),
-                  Text("Location: (" + post['latitude'].toString() + ", " + post['longitude'].toString() + ")", style: TextStyle(fontSize: 15.00,color: Colors.white)),
-                  SizedBox(height:10.00),
-        ]),
-    ),
+          child: Column(children: [
+            SizedBox(height: 50.00),
+            Text(post['quantity'].toString() + " items",
+                style: TextStyle(fontSize: 40.00, color: Colors.white)),
+            SizedBox(height: 75.00),
+            Text(
+                "Location: (" +
+                    post['latitude'].toString() +
+                    ", " +
+                    post['longitude'].toString() +
+                    ")",
+                style: TextStyle(fontSize: 15.00, color: Colors.white)),
+            SizedBox(height: 10.00),
+          ]),
+        ),
       ),
-  );
-
-
-
+    );
   }
-
-
 }
